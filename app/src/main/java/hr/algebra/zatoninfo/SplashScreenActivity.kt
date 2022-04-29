1c23a9843b4ffa9d890bf8b11ec2e7b7c50f294b
Tomo Vodnica <tvodnica@racunarstvo.hr>
2022-04-29 23:13:39 +0200

    Created SplashScreen

---
+++ app/src/main/java/hr/algebra/zatoninfo/SplashScreenActivity.kt
@@ -0,0 +1,40 @@
+package hr.algebra.zatoninfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import hr.algebra.zatoninfo.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showAnimations()
+        redirect()
+    }
+
+    private fun showAnimations() {
+       binding.tvHello.startAnimation(
+           AnimationUtils.loadAnimation(this, R.anim.rotate)
+       )
+
+        binding.tvWelcomeTo.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.slide)
+        )
+
+        binding.tvZaton.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.fadein)
+        )
+
+
+    }
+
+    private fun redirect() {
+    }
+
+}
